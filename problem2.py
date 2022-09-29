c12756796ad0a243150462e4353924829019d06a
Tejaakalathuru <114468602+Tejaakalathuru@users.noreply.github.com>
2022-09-29 12:48:36 +0530

    Create problem2.py

---
+++ problem2.py
@@ -0,0 +1,10 @@
+N = int(input())
+series = 1
+for i in range(N):
+    if series % 5 == 0:
+            series += 2
+    if i == a+1:
+        print(series)
+    else:
+        print(series, ',', end="")
+    series += 0
